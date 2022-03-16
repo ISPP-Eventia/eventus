619acef093a164a436a9172af91e2e5093020aa3
pedalopon <pedropontiga@gmail.com>
2022-03-16 22:22:20 +0100

    #65-refactor: Change createParticipation and remove updateParticipation

--- backend/src/main/java/com/eventus/backend/controllers/ParticipationController.java
+++ backend/src/main/java/com/eventus/backend/controllers/ParticipationController.java
@@ -6,6 +6,7 @@ import com.eventus.backend.models.User;
 import com.eventus.backend.services.EventService;
 import com.eventus.backend.services.ParticipationService;
 import com.eventus.backend.services.UserService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 @RestController
 public class ParticipationController {
@@ -70,35 +72,8 @@ public class ParticipationController {
             User user = this.userService.findUserById(1L).orElse(null);
             if(user!=null&&event!=null){
                 Participation participation=new Participation();
-                participation.setTicket(p.get("ticket"));
-                participation.setPrice(Double.valueOf(p.get("price")));
-                participation.setEvent(event);
-                participation.setUser(user);
-                this.participationService.saveParticipation(participation);
-            }else{
-                return ResponseEntity.notFound().build();
-            }
-
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        }catch(DataAccessException|NullPointerException e){
-            return ResponseEntity.badRequest().build();
-        }
-
-
-    }
-
-    @PutMapping("/participations/{id}")
-    public ResponseEntity<String> updateParticipation(@RequestBody Map<String,String> p,@PathVariable Long id) {
-        try{
-            Participation participation=this.participationService.findParticipationById(id).orElse(null);
-            if(participation==null){
-                return ResponseEntity.notFound().build();
-            }
-            Event event= this.eventService.findById(Long.valueOf(p.get("eventId")));
-            User user = this.userService.findUserById(1L).orElse(null);
-            if(user!=null&&event!=null){
-                participation.setTicket(p.get("ticket"));
-                participation.setPrice(Double.valueOf(p.get("price")));
+                participation.setTicket(RandomStringUtils.randomAlphanumeric(20));
+                participation.setPrice(event.getPrice());
                 participation.setEvent(event);
                 participation.setUser(user);
                 this.participationService.saveParticipation(participation);

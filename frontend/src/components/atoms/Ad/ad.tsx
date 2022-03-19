58e7ac1f44f0b1b859f5d1a8c44d14841a5d714f
Jose Nieves <esoj.nieves@gmail.com>
2022-03-19 21:05:06 +0100

    #67-feat: implement event detail page
    - updated types
    - added map and ad components
    - configured typography, (reduced)

---
+++ frontend/src/components/atoms/Ad/ad.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Component = () => {
+  // TODO
+  return (
+    <div className="flex h-auto w-full items-center justify-center rounded-md bg-black bg-opacity-20">
+      AD PLACEHOLDER
+    </div>
+  );
+};
+
+export default Component;

95aec0d2d3d52fd700f0fa69efc0d8c028930f27
Jose Nieves <esoj.nieves@gmail.com>
2022-03-20 13:33:10 +0100

    #113: simplify api utils structure and re-order types

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,5 @@
 import Axios from "axios";
 
-/* TODO CREATE SESSION UTILS IN ORDER TO LOG IN AND LOGOUT
- import { sessionUtils } from "utils"; */
-
-
 const BASE_URL = window.location.href.includes("localhost")
   ? "http://localhost:3000/"
   : window.location.origin + "/landing";
@@ -12,27 +8,11 @@ const API_URL = BASE_URL + "/api";
 
 export const axios = Axios.create({
   baseURL: API_URL,
-  //withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
   },
 });
-
-// No auth implemented yet (logout)
-//TODO IMPLEMENT SESSION UTILS
-/* axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response?.status === 403 || error.response?.status === 401) {
-      sessionUtils.removeToken();
-      window.location.reload();
-    }
-    return Promise.reject(error);
-  }
-); */
 
 // Error handling interceptor
 axios.interceptors.response.use(
--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,56 @@
-import eventApi from "./eventUsApiUtils"
+import { axios } from "./axios";
+import { EventUs } from "types";
 
-export { eventApi }
+// utitlities
+const mediaApi = {
+  //bulk operations
+  //individual operations
+};
+
+// main entities
+const userApi = {
+  //bulk operations
+  //individual operations
+};
+
+const eventApi = {
+  //bulk operations
+  getEvents: () => axios.get("/events"),
+
+  //individual operations
+  getEvent: (id: number) => axios.get(`/events/${id}`),
+  createEvent: (event: EventUs) => axios.post("/events", event),
+  updateEvent: (event: EventUs) => axios.put(`/events/${event.id}`, event),
+  deleteEvent: (id: number) => axios.delete(`/events/${id}`),
+};
+
+const locationApi = {
+  //bulk operations
+  //individual operations
+};
+
+// relations
+const hostingApi = {
+  //bulk operations
+  //individual operations
+};
+
+const participationApi = {
+  //bulk operations
+  //individual operations
+};
+
+const sponsorshipApi = {
+  //bulk operations
+  //individual operations
+};
+
+export {
+  mediaApi,
+  userApi,
+  eventApi,
+  locationApi,
+  hostingApi,
+  participationApi,
+  sponsorshipApi,
+};

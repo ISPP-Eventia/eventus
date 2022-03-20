5b22793107cf90a2a047b6b2101c68c42d6fe694
pedalopon <pedropontiga@gmail.com>
2022-03-20 14:18:49 +0100

    #73-feat: Added function generateTicket to participationController

--- backend/src/main/java/com/eventus/backend/controllers/ParticipationController.java
+++ backend/src/main/java/com/eventus/backend/controllers/ParticipationController.java
@@ -6,12 +6,28 @@ import com.eventus.backend.models.User;
 import com.eventus.backend.services.EventService;
 import com.eventus.backend.services.ParticipationService;
 import com.eventus.backend.services.UserService;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.LineSeparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.sound.sampled.Line;
+import java.awt.*;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -97,5 +113,246 @@ public class ParticipationController {
         } catch (DataAccessException e) {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/participation/{id}/ticket")
+    public ResponseEntity<byte[]> generateTicket(@PathVariable Long id){
+        ResponseEntity<byte[]> response = null;
+        Participation participation=this.participationService.findParticipationById(id);
+        if(participation!=null){
+            Document document=new Document();
+            try {
+                ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+                PdfWriter.getInstance(document,outputStream);
+                document.open();
+
+                //Image
+                Path path = Paths.get("src/main/resources/com.eventus.backend.controllers/Logo.png");
+                Image img = Image.getInstance(path.toAbsolutePath().toString());
+                img.scalePercent(20);
+                img.setAlignment(Element.ALIGN_CENTER);
+                document.add(img);
+
+                //Tittle
+                Paragraph paragraph=new Paragraph();
+                Font font = FontFactory.getFont(FontFactory.TIMES_BOLD, 50, new BaseColor(28, 69, 135));
+                Chunk chunk = new Chunk("Event", font);
+                paragraph.add(chunk);
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 50, new BaseColor(47, 112, 213));
+                chunk = new Chunk("Us", font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(35);
+                paragraph.setSpacingAfter(10);
+                paragraph.setAlignment(Element.ALIGN_CENTER);
+                document.add(paragraph);
+
+
+                LineSeparator separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(245, 125, 198));
+                separator.setLineWidth(8);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(47, 112, 213));
+                separator.setLineWidth(8);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(80, 182, 107));
+                separator.setLineWidth(8);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(254, 164, 70));
+                separator.setLineWidth(8);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+
+                //Tittle
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 35, new BaseColor(65, 64, 64));
+                chunk = new Chunk(participation.getEvent().getTitle(), font);
+                paragraph=new Paragraph();
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(35);
+                paragraph.setSpacingAfter(10);
+                paragraph.setAlignment(Element.ALIGN_CENTER);
+                document.add(paragraph);
+
+                //Event data
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 25, new BaseColor(47, 112, 213));
+                chunk = new Chunk("Datos del evento", font);
+                chunk.setUnderline(3,-5);
+                paragraph.add(chunk);
+                paragraph.setSpacingAfter(10);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+
+                //Description
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Descripción: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getEvent().getDescription(), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Formatters
+                DateTimeFormatter date=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                DateTimeFormatter hour=DateTimeFormatter.ofPattern("HH:mm");
+
+                //Start date
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Fecha de inicio: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getEvent().getStartDate().format(date), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Start hour
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Hora de inicio: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getEvent().getStartDate().format(hour), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //End date
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Fecha de fin: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getEvent().getEndDate().format(date), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //End hour
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Hora de fin: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getEvent().getEndDate().format(hour), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setSpacingAfter(15);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(245, 125, 198));
+                separator.setLineWidth(3);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(47, 112, 213));
+                separator.setLineWidth(3);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(80, 182, 107));
+                separator.setLineWidth(3);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+                separator = new LineSeparator();
+                separator.setLineColor(new BaseColor(254, 164, 70));
+                separator.setLineWidth(3);
+                chunk = new Chunk(separator);
+                document.add(chunk);
+
+                //Buy data
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 25, new BaseColor(47, 112, 213));
+                chunk = new Chunk("Datos de compra", font);
+                chunk.setUnderline(3,-5);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(15);
+                paragraph.setSpacingAfter(10);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //First name
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Nombre: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getUser().getFirstName(), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Last name
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Apellidos: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getUser().getLastName(), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Buy date
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Fecha de compra: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(participation.getBuyDate().format(date), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Price
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Precio: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(String.valueOf(participation.getPrice()), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                //Ticket id
+                paragraph=new Paragraph();
+                font = FontFactory.getFont(FontFactory.TIMES_BOLD, 18, new BaseColor(65, 64, 64));
+                chunk = new Chunk("Id de ticket: ", font);
+                paragraph.add(chunk);
+                chunk = new Chunk(String.valueOf(participation.getTicket()), font);
+                paragraph.add(chunk);
+                paragraph.setSpacingBefore(5);
+                paragraph.setAlignment(Element.ALIGN_JUSTIFIED);
+                document.add(paragraph);
+
+                document.close();
+
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_PDF);
+                String filename = "ticket.pdf";
+                headers.setContentDispositionFormData(filename, filename);
+                headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+                response = new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+
+            } catch (DocumentException | IOException e) {
+                response = ResponseEntity.badRequest().build();
+            }
+        }else{
+            response = ResponseEntity.badRequest().build();
+        }
+
+
+        return response;
     }
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 UPDATE hibernate_sequence SET next_val=3 LIMIT 1;
 INSERT INTO user(id,first_Name,last_Name) VALUES (1, 'Juan', 'Antonio');
 INSERT INTO user(id,first_Name,last_Name) VALUES (2, 'Rosa', 'Margarita');
-INSERT INTO event(id,description,price,title,user_id) VALUES (1,'Descripccion',10.0,'Evento',1);
+INSERT INTO event(id,description,price,title,user_id,start_Date,end_Date) VALUES (1,'Descripccion',10.0,'Evento',1, "2022-04-23T20:26:41.036121","2022-05-23T20:26:41.036121");

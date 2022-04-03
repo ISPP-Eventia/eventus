7c9cb6b5e8ff9e2971124caefc2c2eac40d84a7d
George.Bogdan <George.Bogdan@acronis.com>
2022-04-03 16:16:53 +0200

    #no-ref-issue - fix: Changed cors filter

--- backend/src/main/java/com/eventus/backend/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/eventus/backend/configuration/SecurityConfiguration.java
@@ -89,17 +89,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     return filter;
   }
 
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("*"));
-    configuration.setAllowedHeaders(Arrays.asList("*"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-  
+
   @Bean
   SimpleUrlAuthenticationSuccessHandler successHandler() {
     final SimpleUrlAuthenticationSuccessHandler successHandler = new SimpleUrlAuthenticationSuccessHandler();

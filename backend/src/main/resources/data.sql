6d62f2c4a314509ab2dd14abc17c7871ddde58be
Jose Nieves <esoj.nieves@gmail.com>
2022-04-06 23:38:01 +0200

    hotfix: add admin payment account

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,7 +29,7 @@ INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date) VAL
 INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date) VALUES (28, 'Laura', 'Ramos','$2a$10$aQ2qGzK5p7gmimjx.JDkU.FvIe6Mo17B6SVSZhCGwNP9MEriYfsJi',false,'moncalvillo2@gmail.com','2000/04/30');
 INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date) VALUES (29, 'Anguela', 'Martin','$2a$10$aQ2qGzK5p7gmimjx.JDkU.FvIe6Mo17B6SVSZhCGwNP9MEriYfsJi',false,'beabeltran2@gmail.com','2000/11/15');
 INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date) VALUES (30, 'Cristina', 'Gutierrez','$2a$10$aQ2qGzK5p7gmimjx.JDkU.FvIe6Mo17B6SVSZhCGwNP9MEriYfsJi',false,'joselu2@gmail.com','2000/06/06');
-INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date) VALUES (11, 'Admin', 'admin','$2a$10$6LWjTo6TUadfX26Qwini2O7ufhcIb0nYM90eLvav5B9wmeF0of5um',true,'admin@gmail.com','2000/03/29');
+INSERT INTO user(id,first_Name,last_Name,password,is_Admin,email,birth_Date,customer_id) VALUES (11, 'Admin', 'admin','$2a$10$6LWjTo6TUadfX26Qwini2O7ufhcIb0nYM90eLvav5B9wmeF0of5um',true,'admin@gmail.com','2000/03/29', 'cus_LSbhTOdH7eOKMS');
 
 INSERT INTO event(id,description,price,title,user_id,start_Date,end_Date,prize) VALUES (1,'Torneo de fútbol sala. Se necesitan 8 equipos para realizar el sorteo de las eliminatorias.',5.0,'Torneo fútbol sala',1, "2022-04-22T20:00:00.000000","2022-04-24T20:00:00.000000",200.0);
 INSERT INTO event(id,description,price,title,user_id,start_Date,end_Date) VALUES (2,'Gran final del carnaval de Cádiz',50.0,'Carnaval de Cádiz',2, "2022-06-21T19:30:00.000000","2022-06-22T19:30:00.000000");

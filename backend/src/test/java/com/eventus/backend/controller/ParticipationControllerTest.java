7c1e40b18513d7c7de948c96c278e6f42bd2db95
pedalopon <pedropontiga@gmail.com>
2022-04-25 20:37:48 +0200

    #275-test: Initial ParticipationControllerTest

---
+++ backend/src/test/java/com/eventus/backend/controller/ParticipationControllerTest.java
@@ -0,0 +1,39 @@
+package com.eventus.backend.controller;
+
+import com.eventus.backend.controllers.ParticipationController;
+import com.eventus.backend.models.Participation;
+import com.eventus.backend.services.EventService;
+import com.eventus.backend.services.ParticipationService;
+import com.eventus.backend.services.StripeService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = ParticipationController.class)
+@ActiveProfiles("test")
+public class ParticipationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ParticipationService participationService;
+    @MockBean
+    private EventService eventService;
+    @MockBean
+    private StripeService stripeService;
+
+    @Test
+    public void testGetParticipationById() throws Exception {
+        Participation participation = new Participation();
+        given(participationService.findParticipationById(1L)).willReturn(participation);
+        MvcResult res = this.mockMvc.perform(MockMvcRequestBuilders.get("/1")).andExpect(status().isOk()).andReturn();
+    }
+}

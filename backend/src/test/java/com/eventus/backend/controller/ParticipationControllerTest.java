fe631d1d3907afcac753041a2f31ead6eb03cf3b
carvilgar1 <carvilgar1@alum.us.es>
2022-04-26 03:28:38 +0200

    Changed test beans

--- backend/src/test/java/com/eventus/backend/controller/ParticipationControllerTest.java
+++ backend/src/test/java/com/eventus/backend/controller/ParticipationControllerTest.java
@@ -1,15 +1,23 @@
 package com.eventus.backend.controller;
 
+import com.eventus.backend.configuration.SecurityConfiguration;
+import com.eventus.backend.configuration.TokenAuthenticationProvider;
 import com.eventus.backend.controllers.ParticipationController;
 import com.eventus.backend.models.Participation;
 import com.eventus.backend.services.EventService;
 import com.eventus.backend.services.ParticipationService;
 import com.eventus.backend.services.StripeService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,8 +25,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = ParticipationController.class)
-@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(ParticipationController.class)
 public class ParticipationControllerTest {
 
     @Autowired

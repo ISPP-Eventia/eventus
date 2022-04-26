9a27df685a7b53a12bbf00313a9ab8c95bc33ea3
pabgonmon2 <pablogonmon@gmail.com>
2022-04-26 20:56:21 +0200

    #275 - test: Fixed controller test

--- backend/src/test/java/com/eventus/backend/controller/ParticipationControllerTest.java
+++ backend/src/test/java/com/eventus/backend/controller/ParticipationControllerTest.java
@@ -1,32 +1,31 @@
 package com.eventus.backend.controller;
 
-import com.eventus.backend.configuration.SecurityConfiguration;
-import com.eventus.backend.configuration.TokenAuthenticationProvider;
-import com.eventus.backend.controllers.ParticipationController;
-import com.eventus.backend.models.Participation;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import com.eventus.backend.services.EventService;
 import com.eventus.backend.services.ParticipationService;
 import com.eventus.backend.services.StripeService;
+import com.eventus.backend.services.UserService;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.token.TokenService;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest(ParticipationController.class)
+import java.util.ArrayList;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ParticipationControllerTest {
 
     @Autowired
@@ -37,11 +36,21 @@ public class ParticipationControllerTest {
     private EventService eventService;
     @MockBean
     private StripeService stripeService;
+    @Autowired
+    private UserService userService;
+
+
+    private String token = "";
+
+    @Before
+    public void setUp() {
+        this.token = userService.login("admin@gmail.com", "admin").get();
+    }
 
     @Test
-    public void testGetParticipationById() throws Exception {
-        Participation participation = new Participation();
-        given(participationService.findParticipationById(1L)).willReturn(participation);
-        MvcResult res = this.mockMvc.perform(MockMvcRequestBuilders.get("/1")).andExpect(status().isOk()).andReturn();
+    public void testFindUserParticipations() throws Exception {
+        // ParticipationService partServ = Mockito.mock(ParticipationService.class);
+        when(participationService.findAllParticipation(PageRequest.of(0, 20000))).thenReturn(new ArrayList<>());
+        this.mockMvc.perform(get("/user/participations").header("Authorization", "Bearer " + token)).andExpect(status().isOk());
     }
 }

4fca2f03976cc51b3a34e3c7e570b4e3b2ecd0d0
carvilgar1 <carvilgar1@us.es>
2022-04-27 20:01:09 +0200

    test: Media test service tests

--- backend/src/main/java/com/eventus/backend/models/EventTag.java
+++ backend/src/main/java/com/eventus/backend/models/EventTag.java
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
 
 @Entity
 public class EventTag {
